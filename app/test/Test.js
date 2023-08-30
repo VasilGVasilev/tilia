b5fe547027fcdb72ffa1b784dcb6d8c2bc64f56a
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-08-30 12:18:46 +0300

    add a testing page

---
+++ app/test/Test.js
@@ -0,0 +1,30 @@
+
+'use client'
+
+import Image from "next/image"
+import { imageLoader } from "../../utils/imgLoader";
+import Maps from "@/components/Location";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+import { SlLocationPin } from 'react-icons/sl'
+import { BsTelephone } from 'react-icons/bs'
+import { TfiEmail } from 'react-icons/tfi'
+
+
+export const revalidate = 0; // revalidate this page every 60 seconds
+
+// todo: small icons dont show
+
+export default function Test() {
+
+
+    return (
+        <>
+
+
+
+        </>
+    )
+}
+
---
+++ app/test/page.js
@@ -0,0 +1,14 @@
+import Test from "./Test";
+
+import { metaData } from "@/utils/metadata";
+
+export const metadata = metaData('Testing')
+
+
+export default function Page() {
+
+    return (
+      <Test></Test>
+    )
+}
+  
