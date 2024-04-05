d4bc4cf9f1be1aff01fa7158896a8f24182f023d
VasilGVasilev <vaskomeil@gmail.com>
2024-04-05 17:08:40 +0300

    Update Apartments.js

--- app/apartments/Apartments.js
+++ app/apartments/Apartments.js
@@ -1,18 +1,26 @@
-'use client'
+"use client";
 
-import Plan from '@/components/Plan';
-import { noto } from '@/utils/fonts';
-import { motion } from 'framer-motion';
-import { container, letterVariant } from '../Home';
-import { useSearchParams } from 'next/navigation';
+import Plan from "@/components/Plan";
+import { noto } from "@/utils/fonts";
+import { motion } from "framer-motion";
+import { container, letterVariant } from "../Home";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export const revalidate = 0; // revalidate this page every 60 seconds
 
-
 export default function Apartments() {
-
     const searchParams = useSearchParams();
-    const bedrooms = searchParams.get('param');
+    const bedrooms = searchParams.get("param");
+    // the below works if we have no subsequent setting of the bedroom type, but we have - it is a filter
+    // const [selectedBedroomType, setSelectedBedroomType] = useState(bedrooms || "all");
+    const [selectedBedroomType, setSelectedBedroomType] = useState("all");
+
+    useEffect(() => {
+        if (bedrooms !== null) {
+            setSelectedBedroomType(bedrooms);
+        }
+    }, [bedrooms]);
 
     return (
         <>
@@ -34,18 +42,14 @@ export default function Apartments() {
                         <motion.span variants={letterVariant}>А</motion.span>
                         <motion.span variants={letterVariant}>Ж</motion.span>
                         <motion.span variants={letterVariant}>И</motion.span>
-
                     </motion.div>
-
                 </div>
             </div>
 
-
             {/* FLOOR PLAN */}
-            <div className='bg-tilia-gray'>
+            <div className="bg-tilia-gray">
                 <Plan></Plan>
             </div>
         </>
-    )
+    );
 }
-
